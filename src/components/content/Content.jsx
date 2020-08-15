85fb06f1e4b4b6dae9343e7da48a5602348d14ef
Tatiana Saucedo <tatijael@gmail.com>
2020-08-15 19:36:11 -0300

    Se modifica content

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -3,7 +3,10 @@ import './Content.css'
 
 const Content = () => (
   <div className="content-container">
-    <p>contenido</p>
+    <p>Enrique I (en inglés moderno, Henry I; c. 1068-1 de diciembre de 1135), 
+      llamado Enrique Beauclerc («Buen Sabio» en francés) por sus intereses 
+      culturales, fue rey de Inglaterra desde 1100 hasta su muerte. Hijo de 
+      Guillermo I el Conquistador, recibió educación por parte de la Iglesia. </p>
   </div>
 )
 
